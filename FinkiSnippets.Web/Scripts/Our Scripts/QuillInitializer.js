c5952201caf4e2cc9b66020c16345ec7d532c6f1
msolev <martin_solev@hotmail.com>
2016-03-01 04:43:26 +0100

    1. Sredeno Lobby
    2. Sreden Editor 80% vo Create Event

---
+++ FinkiSnippets.Web/Scripts/Our Scripts/QuillInitializer.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    var advancedEditor = new Quill('.editor-container', {
+        modules: {
+            'authorship': {
+                authorId: 'advanced',
+                enabled: true
+            },
+            'toolbar': {
+                container: '.toolbar-container'
+            },
+            'link-tooltip': true,
+            'image-tooltip': true,
+            'multi-cursor': true
+        },
+        styles: false,
+        theme: 'snow'
+    });
+});
